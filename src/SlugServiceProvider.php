fecddae084f6f9578d7ffd7e04a2b23e8474821c
Igor Mozgalov <fsmdev@gmail.com>
2020-02-28 08:34:09 +0300

    Add config file

--- src/SlugServiceProvider.php
+++ src/SlugServiceProvider.php
@@ -19,8 +19,13 @@ class SlugServiceProvider extends ServiceProvider
     {
         App::bind('fsmdev_laravel_slug_manager', function()
         {
-            return new Slug();
+            $class = config('laravel_slug.facade_class');
+            return new $class;
         });
+
+        $this->mergeConfigFrom(
+            __DIR__.DIRECTORY_SEPARATOR.'config/laravel_slug.php', 'laravel_slug'
+        );
     }
 
     /**
@@ -34,6 +39,11 @@ class SlugServiceProvider extends ServiceProvider
         $this->publishes([__DIR__.DIRECTORY_SEPARATOR.'migrations/2019_03_12_050059_create_slugs_table.stub'
             => database_path('migrations/'.date('Y_m_d_His').'_create_slugs_table.php'),
         ], 'migration');
+
+        # php artisan vendor:publish --provider="Fsmdev\LaravelSlug\SlugServiceProvider" --tag=config
+        $this->publishes([
+            __DIR__.DIRECTORY_SEPARATOR.'config/laravel_slug.php' => config_path('laravel_slug.php'),
+        ], 'config');
 
         Validator::extend('unique_slug', function($attribute, $value, $parameters)
         {
---
+++ src/config/laravel_slug.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+
+    'facade_class' => 'Fsmdev\LaravelSlug\Models\Managers\Slug',
+
+];
